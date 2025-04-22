dd623a81b1252ec97becf73018ed21787a98facb
Luigi.Labriola <Luigi.Labriola@FSD-2426-12>
2025-04-22 16:31:49 +0200

    Es11

---
+++ lesson/Class.cs
@@ -0,0 +1,40 @@
+﻿namespace lesson
+{
+    public class Class
+    {
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        private string name;
+
+        public ClassMember[] Members
+        {
+            get
+            {
+                return this.members;
+            }
+        }
+
+        private ClassMember[] members;
+
+        public Class(string name, ClassMember[] members)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name cannot be null or empty");
+            }
+            if (members == null || members.Length == 0)
+            {
+                throw new ArgumentException("Members cannot be null or empty");
+            }
+
+            this.name = name;
+            this.members = members;
+        }
+    }
+}
